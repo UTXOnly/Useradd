2c3cd9a0612974d9b9b7f3486ac7d3cb86ee982a
Brian Hartford <49233513+NabismoPrimeTime@users.noreply.github.com>
2020-10-18 08:07:41 -0700

    Added necessary changes to work on CentOS

--- createSKEL.sh
+++ createSKEL.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
+#add groups
+
+addgroup cis245admins
+addgroup cis245developers
+addgroup cis245staff
+addgroup cis245tempemps
+
+# Uncomment this section if you are running this script on CentOS
+
+#groupadd cis245admins
+#groupadd cis245developers
+#groupadd cis245staff
+#groupadd cis245tempemps
+
+
 #Change to home directory and make folders for each group
 cd /home
 mkdir Admins_folder
+chgrp cis245admins Admins_folder
+chmod 660 Admins_folder
 mkdir Dev_folder
+chgrp cis245developers Dev_folder
+chmod 660 Dev_folder
 mkdir Staff_folder
+chgrp cis245staff Staff_folder
+chmod 660 Staff_folder
 mkdir Temp_folder
+chgrp cis245tempemps Temp_folder
+chmod 660 Temp_folder
+mkdir company_public_folder
+chmod 444 company_public_folder
 
 #Change directories and create skeleton directories for each group
 cd /etc/skel
